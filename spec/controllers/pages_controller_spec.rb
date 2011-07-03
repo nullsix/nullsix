b2d1dff3b9b93c2fe406cba9ce00969c44665c62
Kyle Tolle <kyle@nullsix.com>
2011-07-03 00:43:37 -0600

    Added tests for Home and Contact page titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,40 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title = "nullsix | "
+  end
 
   describe "GET 'home'" do
+
+    before(:each) do
+      get :home
+    end
     it "should be successful" do
-      get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      response.should have_selector("title",
+                                    :content => "#{@base_title}Home")
     end
   end
 
   describe "GET 'contact'" do
+
+    before(:each) do
+      get :contact
+    end
+
     it "should be successful" do
-      get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      response.should have_selector("title",
+                                    :content => "#{@base_title}Contact")
     end
   end
 
