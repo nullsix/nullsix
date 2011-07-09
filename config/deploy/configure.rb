e57f465efd6284ae0a3ae154755dd2a790ca5148
Kyle Tolle <kyle@nullsix.com>
2011-07-09 13:28:38 -0600

    The last commit actually didn't remove the public index

--- config/deploy/configure.rb
+++ config/deploy/configure.rb
@@ -3,9 +3,9 @@ namespace :configure do
   # after deploy, this will link the database.yml
   task :link_database_yml do
     #puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{release_path}"
- #   run <<-CMD
-#ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml
-#CMD
+    run <<-CMD
+ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml
+CMD
   end
 
   # this will create shared directories for gems
@@ -19,12 +19,12 @@ CMD
 end
 
 namespace :bundlr do
-  task :redeploy_gems, :roles => :app, :except => {:no_release => true} do
-    run %{
-cd #{release_path} &&
-ln -nfs #{shared_path}/gems/gems #{release_path}/gems/gems &&
-ln -nfs #{shared_path}/gems/specifications #{release_path}/gems/specifications &&
-thor merb:gem:redeploy
-}
-  end
+#  task :redeploy_gems, :roles => :app, :except => {:no_release => true} do
+#    run %{
+#cd #{release_path} &&
+#ln -nfs #{shared_path}/gems/gems #{release_path}/gems/gems &&
+#ln -nfs #{shared_path}/gems/specifications #{release_path}/gems/specifications &&
+#thor merb:gem:redeploy
+#}
+#  end
 end
