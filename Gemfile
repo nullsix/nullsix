3d3283fa4a461b16185a68cdf6407659f758c86c
Kyle Tolle <kyle@nullsix.com>
2012-05-07 21:56:11 -0600

    Successful Capistrano deploy to inthom.com

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.1.0'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'capistrano'
+gem 'capistrano-ext'
 
 gem 'rack', '1.3.2'
 gem 'sqlite3'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,30 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
 require 'bundler/capistrano'
 
 # Servers
 #production
-set :domain, 'kernelriot.com'
+set :domain, 'inthom.com'
 role :app, domain
 role :web, domain
-set :gituser, 'kernelRiot'
-set :user, 'nsinst'
+set :gituser, 'git'
+set :user, 'kyle'
 role :db, domain, :primary => true
 
+set :rvm_type, :system
+set :rvm_ruby_string, '1.9.3@inthom-nullsix'
 
 # Application
 set :application, "nullsix"
-set :deploy_to, "/home/#{user}/www/#{domain}"
+set :deploy_to, "/srv/www/#{domain}"
 
 # Settings
 set :use_sudo, false
 #default_run_options[:pty] = true
-set :keep_releases, 2
+#set :keep_releases, 2
 #set :chmod755, "app config db lib public vendor script script/* public/disp*"
-ssh_options[:keys] = %w(~/.ssh/authorized_keys)
+ssh_options[:forward_agent] = true
+#ssh_options[:keys] = %w(~/.ssh/authorized_keys)
 #ssh_options[:keys] = %w(/etc/ssh/ssh_host_rsa_key)
 
 # Includes
@@ -27,46 +32,13 @@ load 'config/deploy/configure'
 
 # Git
 set :scm, :git
-set :branch, 'master'
-set :repository,  "#{gituser}@#{domain}:~/repos/#{application}.git"
-set :deploy_via, :remote_cache
+set :branch, 'barebones'
+set :repository,  "#{gituser}@bitbucket.org:kyletolle/#{application}.git"
+#set :deploy_via, :remote_cache
 #set :scm_command, "~/packages/bin/git"
 #set :local_scm_command, "/opt/local/bin/git"
 #set :scm_verbose, true
-set :git_shallow_clone, 1
-set :git_enable_submodules, 1
 
-#############################################################
-# Post Deploy Hooks
-#############################################################
-
-namespace :deploy do
-
-  desc "Runs after every successful deployment"
-  task :after_default do
-    cleanup #removes the old deploys
-  end
-
-  #after "deploy:setup", "configure:create_shared_directories"
-  after "deploy:update_code", "configure:link_database_yml"
-  #after "deploy:symlink", "bundlr:redeploy_gems"
-end
-
-#############################################################
-# Passenger
-#############################################################
-
-namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-end
 
 # deploy config
 
@@ -84,14 +56,14 @@ end
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-## If you are using Passenger mod_rails uncomment this:
-#namespace :deploy do
-##  task :restart do
-##    run "touch #{current_path}/tmp/restart.txt"
-##  end
-#  task :start do ; end
-#  task :stop do ; end
-#  task :restart, :roles => :app, :except => { :no_release => true } do
-#    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-#  end
-#end
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
